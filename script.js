3a14d9f4996130f908e1e293243288c997c8aad1
majidjuniorZ <98183646+majidjuniorZ@users.noreply.github.com>
2023-11-11 03:18:07 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+window.onload = function() {
+    // Example: Log a message when the contact section is in view
+    document.getElementById('contact').addEventListener('scroll', function() {
+        console.log('You are viewing the contact section');
+    });
+};
